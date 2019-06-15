1ebdc572d4446a2f626c52e539cf243e5711c1f6
Arafat Mohammed <arafat@dais.com>
2019-06-15 13:03:19 -0500

    Added items in a local data.py and listing them from the Submit button

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from data import Items
 
 app = Flask(__name__)
 
+Items = Items()
 
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
+@app.route('/submit')
+def submit():
+    return render_template('items.html', items = Items)
+
+@app.route('/item/<string:id>')
+def item(id):
+    return render_template('item.html', id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,39 @@
+def Items():
+    items = [
+        {
+            'id': 1,
+            'title': 'Couch',
+            'description': 'Lorem ipsum dolor sit amet, feugiat nonumes usu ne. Justo dolorem consequuntur est te. Mei affert fabulas ancillae ex, sit legere ancillae philosophia eu. Eius semper eligendi ei mel, id ipsum sanctus duo. Cu vim inimicus deterruisset, his ex semper nusquam epicuri.',
+            'owner': 'Arafat',
+            'create_date': '06-15-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Queen Bed',
+            'description': 'Lorem ipsum dolor sit amet, feugiat nonumes usu ne. Justo dolorem consequuntur est te. Mei affert fabulas ancillae ex, sit legere ancillae philosophia eu. Eius semper eligendi ei mel, id ipsum sanctus duo. Cu vim inimicus deterruisset, his ex semper nusquam epicuri.',
+            'owner': 'Qamber',
+            'create_date': '06-15-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Shoe Lace',
+            'description': 'Lorem ipsum dolor sit amet, feugiat nonumes usu ne. Justo dolorem consequuntur est te. Mei affert fabulas ancillae ex, sit legere ancillae philosophia eu. Eius semper eligendi ei mel, id ipsum sanctus duo. Cu vim inimicus deterruisset, his ex semper nusquam epicuri.',
+            'owner': 'Mehdi',
+            'create_date': '06-15-2019'
+        },
+        {
+            'id': 4,
+            'title': 'Lawn Mower',
+            'description': 'Lorem ipsum dolor sit amet, feugiat nonumes usu ne. Justo dolorem consequuntur est te. Mei affert fabulas ancillae ex, sit legere ancillae philosophia eu. Eius semper eligendi ei mel, id ipsum sanctus duo. Cu vim inimicus deterruisset, his ex semper nusquam epicuri.',
+            'owner': 'Jose',
+            'create_date': '06-15-2019'
+        },
+        {
+            'id': 5,
+            'title': 'Drill Machine',
+            'description': 'Lorem ipsum dolor sit amet, feugiat nonumes usu ne. Justo dolorem consequuntur est te. Mei affert fabulas ancillae ex, sit legere ancillae philosophia eu. Eius semper eligendi ei mel, id ipsum sanctus duo. Cu vim inimicus deterruisset, his ex semper nusquam epicuri.',
+            'owner': 'Raja',
+            'create_date': '06-15-2019'
+        }
+    ]
+    return items
